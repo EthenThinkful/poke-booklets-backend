8842caafbf0a2ff85d3e6118859fc050b054effb
Ethen Roth <Ethenfm@gmail.com>
2023-08-05 23:04:34 -0600

    postgres config

--- src/main/java/com/ethenthinkful/pokemonapijectapi/model/User.java
+++ src/main/java/com/ethenthinkful/pokemonapijectapi/model/User.java
@@ -9,8 +9,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "user")
 public class User {
 
 	@Id
