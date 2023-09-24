c1330f585856fe0381a10b6281eda7aaa1fe2bf1
EthenThinkful <beatsby3t@gmail.com>
2023-09-24 17:30:17 -0600

    add delete for individual cards

--- src/main/java/com/ethenthinkful/pokemonapijectapi/controllers/CardDataController.java
+++ src/main/java/com/ethenthinkful/pokemonapijectapi/controllers/CardDataController.java
@@ -2,14 +2,22 @@ package com.ethenthinkful.pokemonapijectapi.controllers;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.ethenthinkful.pokemonapijectapi.dto.CardDataRequest;
 import com.ethenthinkful.pokemonapijectapi.model.CardData;
 import com.ethenthinkful.pokemonapijectapi.model.UserData;
 import com.ethenthinkful.pokemonapijectapi.repos.CardDataRepository;
 import com.ethenthinkful.pokemonapijectapi.repos.UserDataRepository;
-import com.ethenthinkful.pokemonapijectapi.service.CardService;
 
 
 @RestController
@@ -28,12 +36,26 @@ public class CardDataController {
 
 	@PostMapping("/cards")
 	public CardData saveCardData(@RequestBody CardDataRequest request) {
-		// System.out.println("Hello! you've hit /cards!");
+		System.out.println("OMFG YOU HIT ME BRUH");
 		UserData user = userDataRepository.findById(request.getUserDataId()).get();
 		CardData cardData = new CardData();
 		cardData.setPokemonCard(request.getPokemonCard());
 		cardData.setUserData(user);
 		return cardDataRepository.save(cardData);
+	}
+
+	@DeleteMapping("/cards/{id}")
+	public ResponseEntity<String> deleteCardData(@PathVariable("id") int id) {
+		System.out.println("OW THAT HURT YOU HIT MY DELETE");
+		// Check if the entity exists in the database
+        if (!cardDataRepository.existsById(id)) {
+            return new ResponseEntity<>("Entity not found", HttpStatus.NOT_FOUND);
+        }
+
+        // If the entity exists, delete it
+        cardDataRepository.deleteById(id);
+
+        return new ResponseEntity<>("Entity deleted successfully", HttpStatus.OK);
 	}
 
 	// @GetMapping("/cards/{userDataId}/{pokemonCard}")
@@ -47,7 +69,6 @@ public class CardDataController {
 	@RequestMapping(value="/cards/{userDataId}", method=RequestMethod.GET)
 	public List<CardData> getSingleCardData(@PathVariable("userDataId") int userDataId) {
 		//return CardService.getCardById(userDataId);
-
 		List<CardData> cardDataList = cardDataRepository.findByUserdataId(userDataId);
 		// System.out.println("Hello! you've hit /cards!/userID (after the query)");
 		// System.out.println(cardDataList);
